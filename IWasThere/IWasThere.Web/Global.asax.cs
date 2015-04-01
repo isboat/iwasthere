c32482c078c87718603165ceabec167e584228f2
isaac boateng <iboateng@sportingsolutions.com>
2015-04-01 10:57:45 +0100

    Removed generated files

--- IWasThere/IWasThere.Web/Global.asax.cs
+++ IWasThere/IWasThere.Web/Global.asax.cs
@@ -16,8 +16,6 @@ namespace IWasThere.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
